c85cba86baeaa2394548cf9e08c5f282f041d8f8
Gai Takakura <g.takakura@outlook.com>
2019-05-11 04:12:41 +0900

    カウンターマネージャーを作成。

---
+++ Assets/Scripts/CounterManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterManager : MonoBehaviour
+{
+	//--------------------------------------------------
+	// フィールド
+	[SerializeField]
+	private Button _addCounterButton;
+	[SerializeField]
+	private GameObject _createTarget;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		if (_addCounterButton != null) _addCounterButton.onClick.AddListener(() => Debug.Log("カウンター生成"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
