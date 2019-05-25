7b527e0154af86472a0a95b532e68e39714ed7d5
Gai Takakura <g.takakura@outlook.com>
2019-05-26 00:51:01 +0900

    タグ追加処理を実装。実行時のみ有効なタグの追加が可能に。

--- Assets/Scripts/TagHelper.cs
+++ Assets/Scripts/TagHelper.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class TagHelper : MonoBehaviour
+/// <summary>
+/// タグ操作補助クラス
+/// </summary>
+public class TagHelper
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	/// <summary>
+	/// 実行時にのみ有効なタグを追加する。
+	/// </summary>
+	/// <param name="setTagName">設定したいタグ名。</param>
+	public static void AddTag(string setTagName)
+	{
+		// タグマネージャーアセットを取得
+		var tagManager = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		if ((tagManager != null) && (tagManager.Length > 0))
+		{
+			// タグプロパティを取得
+			var serializedObject = new SerializedObject(tagManager[0]);
+			var tagsProperty = serializedObject.FindProperty("tags");
+
+			// タグが既に登録されていないか確認
+			for (int i = 0; i < tagsProperty.arraySize; ++i)
+			{
+				if (tagsProperty.GetArrayElementAtIndex(i).stringValue == setTagName) return;
+			}
+
+			// タグ登録
+			var index = tagsProperty.arraySize;
+			tagsProperty.InsertArrayElementAtIndex(index);
+			tagsProperty.GetArrayElementAtIndex(index).stringValue = setTagName;
+
+			// 更新を保存
+			serializedObject.ApplyModifiedProperties();
+			serializedObject.Update();
+		}
+	}
 }
